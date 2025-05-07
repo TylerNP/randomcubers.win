8f6b68119a656fdff85b93370ef10bbf11d15ec2
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-05-07 11:19:53 -0700

    fix: update vite config for hosting

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,21 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   build: {
-    target: 'esnext',
     outDir: 'dist',
-    lib: {
-      entry: 'src/main.tsx',   // or app.tsx depending on your entry
-      formats: ['es'],
-      fileName: () => 'index.js',
-    },
-    rollupOptions: {
-      output: {
-        entryFileNames: 'index.js',
-        manualChunks: undefined,
-      },
-    },
-    minify: true,
-    emptyOutDir: true,
   },
   plugins: [react()],
 })
