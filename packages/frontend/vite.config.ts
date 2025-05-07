7622c635739fdae1d4ba3f944ed8a62850e3d6f0
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-05-07 08:49:30 -0700

    feat: updated vite config for deployments

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    lib: {
+      entry: 'src/main.tsx',   // or app.tsx depending on your entry
+      formats: ['es'],
+      fileName: () => 'worker.js',
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: 'worker.js',
+        manualChunks: undefined,
+      },
+    },
+    minify: true,
+    emptyOutDir: true,
+  },
   plugins: [react()],
 })
