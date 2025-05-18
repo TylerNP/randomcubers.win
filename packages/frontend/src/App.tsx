5c1d1de13f05e5690ef8a4b43ff020cb06121e58
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-05-17 19:41:06 -0700

    feat: practice with react components

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [bal, setBal] = useState(1000);
+  const [winnings, setWinning] = useState(0);
 
   return (
     <>
@@ -16,6 +18,9 @@ function App() {
         </button>
       </div>
       <div className="name">
+        <h2>
+          Hello
+        </h2>
         <form>
           <label >First name:</label><br/>
           <input type="text" id="fname" name="fname"/><br/>
@@ -24,8 +29,42 @@ function App() {
           <input type="submit" value="Submit"/>
         </form>
       </div>
+      <div>
+        <div>
+            Your Balance ${bal.toFixed(2)} 
+        </div>
+        <button onClick={() => {
+            if (bal > 0) {
+            const val = Math.round(100000*(Math.random()-0.7))/100;
+            setWinning(() => val);
+            setBal((curr) => curr+val);
+          } else {
+            return;
+          }
+        }}>
+          You Win
+        </button>
+        <WinText bal={bal} winnings={winnings}/>
+      </div>
     </>
-  )
+  );
+}
+
+interface winProp {
+  bal: number,
+  winnings: number,
+}
+
+function WinText(props: winProp) {
+  if (props.bal > 0) {
+    return <h3>
+      ${props.winnings.toFixed(2)}
+    </h3>;
+  } else {
+    return <h3>
+      Not enough money
+    </h3>;
+  }
 }
 
 export default App
